1fb60541ec45a823e03ba20c1f5790bb65b16c75
Kishan Somani <ksoman@sapient.com>
2018-01-10 13:25:28 +0530

    Initial Commit | login function

---
+++ ui/src/components/userDetail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import IconButton from 'material-ui/IconButton';
+import Input, { InputLabel, InputAdornment } from 'material-ui/Input';
+import { FormControl, FormHelperText } from 'material-ui/Form';
+import Visibility from 'material-ui-icons/Visibility';
+import VisibilityOff from 'material-ui-icons/VisibilityOff';
+import Button from 'material-ui/Button';
+
+class userDetail extends React.Component {
+	render() {
+    return (
+      <div>
+        
+      </div>
+    );
+	}
+}
